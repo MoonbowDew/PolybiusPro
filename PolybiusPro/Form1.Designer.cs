ad28e54615568d037fadbb49386804f81e841584
Andrushka <rulerrulerhigh4seeker4seeker@gmail.com>
2016-11-26 00:04:46 +0300

    Project adding
    Adding the project

---
+++ PolybiusPro/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace PolybiusPro
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.EncryptedBox = new System.Windows.Forms.TextBox();
+            this.DecryptedBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.EncryptButton = new System.Windows.Forms.Button();
+            this.DecryptButton = new System.Windows.Forms.Button();
+            this.BoxCleaner = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // EncryptedBox
+            // 
+            this.EncryptedBox.Location = new System.Drawing.Point(12, 46);
+            this.EncryptedBox.Multiline = true;
+            this.EncryptedBox.Name = "EncryptedBox";
+            this.EncryptedBox.Size = new System.Drawing.Size(191, 217);
+            this.EncryptedBox.TabIndex = 0;
+            // 
+            // DecryptedBox
+            // 
+            this.DecryptedBox.Location = new System.Drawing.Point(283, 46);
+            this.DecryptedBox.Multiline = true;
+            this.DecryptedBox.Name = "DecryptedBox";
+            this.DecryptedBox.Size = new System.Drawing.Size(191, 217);
+            this.DecryptedBox.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(228, 148);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 29);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "⇔";
+            // 
+            // EncryptButton
+            // 
+            this.EncryptButton.Location = new System.Drawing.Point(61, 282);
+            this.EncryptButton.Name = "EncryptButton";
+            this.EncryptButton.Size = new System.Drawing.Size(75, 23);
+            this.EncryptButton.TabIndex = 3;
+            this.EncryptButton.Text = "Encrypt";
+            this.EncryptButton.UseVisualStyleBackColor = true;
+            this.EncryptButton.Click += new System.EventHandler(this.EncryptButton_Click);
+            // 
+            // DecryptButton
+            // 
+            this.DecryptButton.Location = new System.Drawing.Point(328, 282);
+            this.DecryptButton.Name = "DecryptButton";
+            this.DecryptButton.Size = new System.Drawing.Size(75, 23);
+            this.DecryptButton.TabIndex = 4;
+            this.DecryptButton.Text = "Decrypt";
+            this.DecryptButton.UseVisualStyleBackColor = true;
+            this.DecryptButton.Click += new System.EventHandler(this.DecryptButton_Click);
+            // 
+            // BoxCleaner
+            // 
+            this.BoxCleaner.Location = new System.Drawing.Point(170, 359);
+            this.BoxCleaner.Name = "BoxCleaner";
+            this.BoxCleaner.Size = new System.Drawing.Size(165, 23);
+            this.BoxCleaner.TabIndex = 5;
+            this.BoxCleaner.Text = "Clear both boxes";
+            this.BoxCleaner.UseVisualStyleBackColor = true;
+            this.BoxCleaner.Click += new System.EventHandler(this.BoxCleaner_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(58, 19);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "For encrypting";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(342, 19);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(74, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "For decrypting";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(9, 342);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(155, 78);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "*Allowed letters:\r\n[a-z],  [A-Z]\r\n[а-я] (ru, ua),  [А-Я] (ru, ua),\r\nNumbers.\r\n~`!" +
+    "@#%^&*()+-*/_\\|<>;:?.,[]{}$♂\r\nSpace, carriage return.";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(486, 429);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.BoxCleaner);
+            this.Controls.Add(this.DecryptButton);
+            this.Controls.Add(this.EncryptButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.DecryptedBox);
+            this.Controls.Add(this.EncryptedBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox EncryptedBox;
+        private System.Windows.Forms.TextBox DecryptedBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button EncryptButton;
+        private System.Windows.Forms.Button DecryptButton;
+        private System.Windows.Forms.Button BoxCleaner;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ PolybiusPro/Form1.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace PolybiusPro
+{
+    public partial class Form1 : Form
+    {
+        static char[] Letters = new char[]
+        {
+            'а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л',
+            'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш',
+            'щ', 'ь', 'ы', 'ъ', 'э', 'ю', 'я', 'А', 'Б', 'В', 'Г', 'Д', 'Е',
+            'Ё', 'Ж', 'З', 'И', 'Й', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С',
+            'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ь', 'Ы', 'Ъ', 'Э', 'Ю',
+            'Я', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+            'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
+            'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
+            'M', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+            '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '-', '*',
+            '/', '^', '%', '=', '#','\\', '.', ',', ' ', '_', '?', '!', '@',
+            ':', '<', '>', '|', ';', '(', ')', '&', '№', '$', '"', '~', '`',
+            '\r','\n','{', '}', '[', ']', '♂', 'ї', 'Ї', 'є', 'Є', 'ґ', 'Ґ'
+        };
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void EncryptButton_Click(object sender, EventArgs e)
+        {
+            if (EncryptedBox.Text == String.Empty)
+                EncryptedBox.Text = "first, type your message";
+            else
+            {
+                DecryptedBox.Text = String.Empty;
+                DecryptedBox.Text = EncryptData(EncryptedBox.Text);
+            }
+
+        }
+
+        static string EncryptData(string TypedMessage)
+        {
+            int[,] columnsAndRowsArg = CalculateColumnsAndRows(TypedMessage);
+            char[] outstr = new char[TypedMessage.Length];
+            int[] indexes = new int[TypedMessage.Length];
+            int[,] localIntsRight = new int[TypedMessage.Length, 2];
+            int[] asistant = new int[TypedMessage.Length * 2];
+            for (int i = 0; i < TypedMessage.Length; i++)
+            {
+                asistant[i] = columnsAndRowsArg[i, 0];
+            }
+
+            for (int i = 0; i < TypedMessage.Length; i++)
+            {
+                asistant[i + TypedMessage.Length] = columnsAndRowsArg[i, 1];
+            }
+
+            int k = 1;
+            for (int i = 0; i < TypedMessage.Length; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    localIntsRight[i, j] = asistant[k - 1]; //Упорядочить
+                    k++;
+                }
+
+            }
+
+
+            for (int i = 0; i < TypedMessage.Length; i++)
+            {
+                indexes[i] = localIntsRight[i, 0] * 13 - (13 - localIntsRight[i, 1]);
+                outstr[i] = Convert.ToChar(Letters[(indexes[i] - 1)]);
+            }
+            string tmpString = new string(outstr);
+
+            return tmpString;
+        }
+
+        static private int[,] CalculateColumnsAndRows(string TypedMessage)
+        {
+            char[] localTypedMessage = new char[TypedMessage.Length];
+            for (int j = 0; j < TypedMessage.Length; j++)
+            {
+                localTypedMessage[j] = TypedMessage[j];
+            }
+
+            int[,] arrayOfColumnsAndRows = new int[TypedMessage.Length, 2];
+            int k;
+            for (int j = 0; j < TypedMessage.Length; j++) //Column section
+            {
+                k = 1;
+                while (localTypedMessage[j] != Letters[k - 1])
+                {
+                    k++;
+                }
+
+                switch (k % 13)                        //Column section
+                {
+                    case 0:
+                        arrayOfColumnsAndRows[j, 1] = 13;
+                        break;
+                    case 1:
+                        arrayOfColumnsAndRows[j, 1] = 1;
+                        break;
+                    case 2:
+                        arrayOfColumnsAndRows[j, 1] = 2;
+                        break;
+                    case 3:
+                        arrayOfColumnsAndRows[j, 1] = 3;
+                        break;
+                    case 4:
+                        arrayOfColumnsAndRows[j, 1] = 4;
+                        break;
+                    case 5:
+                        arrayOfColumnsAndRows[j, 1] = 5;
+                        break;
+                    case 6:
+                        arrayOfColumnsAndRows[j, 1] = 6;
+                        break;
+                    case 7:
+                        arrayOfColumnsAndRows[j, 1] = 7;
+                        break;
+                    case 8:
+                        arrayOfColumnsAndRows[j, 1] = 8;
+                        break;
+                    case 9:
+                        arrayOfColumnsAndRows[j, 1] = 9;
+                        break;
+                    case 10:
+                        arrayOfColumnsAndRows[j, 1] = 10;
+                        break;
+                    case 11:
+                        arrayOfColumnsAndRows[j, 1] = 11;
+                        break;
+                    case 12:
+                        arrayOfColumnsAndRows[j, 1] = 12;
+                        break;
+                }
+
+                if (k >= 1 && 13 >= k)           //Row section
+                    arrayOfColumnsAndRows[j, 0] = 1;
+                else if (k >= 14 && 26 >= k)
+                    arrayOfColumnsAndRows[j, 0] = 2;
+                else if (k >= 27 && 39 >= k)
+                    arrayOfColumnsAndRows[j, 0] = 3;
+                else if (k >= 40 && 52 >= k)
+                    arrayOfColumnsAndRows[j, 0] = 4;
+                else if (k >= 53 && 65 >= k)
+                    arrayOfColumnsAndRows[j, 0] = 5;
+                else if (k >= 66 && 78 >= k)
+                    arrayOfColumnsAndRows[j, 0] = 6;
+                else if (k >= 79 && 91 >= k)
+                    arrayOfColumnsAndRows[j, 0] = 7;
+                else if (k >= 92 && 104 >= k)
+                    arrayOfColumnsAndRows[j, 0] = 8;
+                else if (k >= 105 && 117 >= k)
+                    arrayOfColumnsAndRows[j, 0] = 9;
+                else if (k >= 118 && 130 >= k)
+                    arrayOfColumnsAndRows[j, 0] = 10;
+                else if (k >= 131 && 143 >= k)
+                    arrayOfColumnsAndRows[j, 0] = 11;
+                else if (k >= 144 && 156 >= k)
+                    arrayOfColumnsAndRows[j, 0] = 12;
+                else
+                    arrayOfColumnsAndRows[j, 0] = 13;
+
+            }
+            return arrayOfColumnsAndRows;
+        }
+
+        private void BoxCleaner_Click(object sender, EventArgs e)
+        {
+            DecryptedBox.Text = String.Empty;
+            EncryptedBox.Text = String.Empty;
+        }
+
+        private void DecryptButton_Click(object sender, EventArgs e)
+        {
+            if (DecryptedBox.Text == String.Empty)
+                DecryptedBox.Text = "first, type your message";
+            else
+            {
+                EncryptedBox.Text = String.Empty;
+                EncryptedBox.Text = DecryptData(DecryptedBox.Text);
+            }
+
+        }
+
+        static string DecryptData(string TypedMessage)
+        {
+            int[,] DecrColumnsAndRowsArg = CalculateColumnsAndRows(TypedMessage);
+            char[] outstr = new char[TypedMessage.Length];
+            int[] indexes = new int[TypedMessage.Length];
+            int[,] localInts = new int[TypedMessage.Length, 2];
+
+            int[,] localIntsRight = new int[TypedMessage.Length, 2];
+            int[] asistant = new int[TypedMessage.Length * 2];
+            int u = 0;
+
+            for (int i = 0; i < TypedMessage.Length; i++)
+            {
+                for (int z = 0; z < 2; z++)
+                {
+                    asistant[u++] = DecrColumnsAndRowsArg[i, z];
+                }
+            }
+
+
+            int k = 1;
+            for (int i = 0; i < TypedMessage.Length; i++)
+            {
+                localIntsRight[i, 0] = asistant[i]; //order the symbols
+                localIntsRight[i, 1] = asistant[i + TypedMessage.Length];
+
+            }
+
+            for (int i = 0; i < TypedMessage.Length; i++)
+            {
+                indexes[i] = localIntsRight[i, 0] * 13 - (13 - localIntsRight[i, 1]);
+                outstr[i] = Convert.ToChar(Letters[(indexes[i] - 1)]);
+            }
+
+            string tmpString = new string(outstr);
+
+            return tmpString;
+        }
+    }
+}
